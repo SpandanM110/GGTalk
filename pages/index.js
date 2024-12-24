b33742035eb2e06e5a409be75a60e106116fa69b
SpandanM110 <spandanmukherjee2003@gmail.com>
2024-12-24 20:18:12 +0530

    Update on pages

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import 'regenerator-runtime/runtime';
+import ConversationPage from '../components/ConversationPage';
+
+const HomePage = () => {
+  return (
+    <div>
+      <ConversationPage />
+    </div>
+  );
+};
+
+export default HomePage;
